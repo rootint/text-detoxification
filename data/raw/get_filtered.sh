cb9c03a17c56e8bb2f5a6918633d07e6ad7deae3
RandoM <danik.tim@gmail.com>
2023-11-05 02:22:14 +0300

    Create data_creation.py

--- data/raw/get_filtered.sh
+++ data/raw/get_filtered.sh
@@ -1,1 +1,13 @@
-wget https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip
+#!/bin/bash
+
+# The URL to download the zip file from
+url="https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip"
+
+# The output folder
+output_folder="."
+
+# Use wget to download the file
+wget "$url" -P "$output_folder"
+
+# Unzip the file in the output folder
+unzip -o "$output_folder/filtered_paranmt.zip" -d "$output_folder"
---
+++ src/data/data_creation.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+
+def preprocess(path="../../data/raw/filtered.tsv"):
+    df = pd.read_csv(path, sep="\t")
+
+    # Cleaning data
+    print("Cleaning data...")
+    df.drop(columns=["Unnamed: 0"], inplace=True)
+    df.rename(columns={"lenght_diff": "length_diff"}, inplace=True)
+
+    # Lowercasing and removing special chars
+    print("Lowercasing and removing special chars...")
+    df["reference"] = (
+        df["reference"]
+        .str.lower()
+        .str.replace(r"[^a-z0-9\s]", "", regex=True)
+        .str.strip()
+    )
+    df["translation"] = (
+        df["translation"]
+        .str.lower()
+        .str.replace(r"[^a-z0-9\s]", "", regex=True)
+        .str.strip()
+    )
+
+    # Dropping unnecessary columns
+    print("Dropping unnecessary columns...")
+    filtered_data = df.drop(["similarity", "length_diff"], axis=1)
+
+    # Swapping reference and translation where needed
+    print("Swapping reference and translation where needed...")
+    filtered_data_swapped = filtered_data.copy()
+    needs_swap = filtered_data_swapped["ref_tox"] < filtered_data_swapped["trn_tox"]
+    filtered_data_swapped.loc[
+        needs_swap, ["reference", "translation"]
+    ] = filtered_data_swapped.loc[needs_swap, ["translation", "reference"]].values
+    filtered_data_swapped.loc[
+        needs_swap, ["ref_tox", "trn_tox"]
+    ] = filtered_data_swapped.loc[needs_swap, ["trn_tox", "ref_tox"]].values
+
+    # Dropping duplicates
+    print("Dropping duplicates...")
+    filtered_data_swapped = filtered_data_swapped.drop_duplicates()
+
+    # Keeping rows with high toxicity threshold
+    print("Keeping rows with high toxicity threshold...")
+    high_toxicity_threshold = 0.6
+    filtered_data_diff = filtered_data_swapped.copy()
+    filtered_data_diff["tox_diff"] = (
+        filtered_data_diff["ref_tox"] - filtered_data_diff["trn_tox"]
+    ).abs()
+    final_data = filtered_data_diff[
+        filtered_data_diff["tox_diff"] > high_toxicity_threshold
+    ].sort_values(by="ref_tox", ascending=True)
+
+    # Dropping rows with more that have strings longer than 256 chars
+    print("Dropping rows with more that have strings longer than 256 chars...")
+    final_data["reference_length"] = final_data["reference"].apply(len)
+    final_data["translation_length"] = final_data["translation"].apply(len)
+    data = final_data[
+        (final_data["reference_length"] <= 256)
+        & (final_data["translation_length"] <= 256)
+    ]
+    training_data = data.drop(
+        ["reference_length", "translation_length", "tox_diff", "ref_tox", "trn_tox"],
+        axis=1,
+    )
+
+    # Saving and removing NaNs
+    print("Saving and removing NaNs...")
+    training_data.to_csv("training_data.csv", index=False)
+    dff = pd.read_csv(
+        "training_data.csv",
+        dtype={"toxic_column": str, "nontoxic_column": str},
+    )
+    dff.dropna(inplace=True)
+    dff.to_csv("training_data.csv", index=False)
+    print("Done!")
+
+
+if __name__ == "__main__":
+    preprocess()
